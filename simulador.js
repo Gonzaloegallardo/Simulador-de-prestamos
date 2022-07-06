db456a28504ae7ad77648e8785eaec872c900757
gonza gallardo <gonzaloegallardo@gmail.com>
2022-07-06 15:29:31 -0300

    agregando arreglos al proyecto

--- simulador.js
+++ simulador.js
@@ -1,35 +1,53 @@
 let limite_prestamo = 0
-function informar_usuario (){
-    alert("tu limite es de: " + limite_prestamo)
+class usuario{
+    constructor(nombre, apellido,ocupacion){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.ocupacion = ocupacion;
+    }
 }
+function agregar_usuario(){
+    console.log("hola ", this.nombre , this.apellido  ,"tu ocupación es ", ocupacion,  " por lo tanto tu limite de prestamo es de :" ,limite_prestamo)
+    let nuevo_usuario = new usuario(this.nombre, this.apellido, this.ocupacion)
+    lista_usuarios.push (nuevo_usuario)
+}
+let lista_usuarios = [];
 
-ocupacion = prompt("ingresa tu ocupacion")
+for(let i=0 ; i < 3; i++){
+this.nombre = prompt("ingrese su nombre")
+this.apellido = prompt("ingrese su apellido")
+this.ocupacion = prompt("ingrese su ocupacion")
 
 if(ocupacion == "estudiante"){
     limite_prestamo = 20000;
-    informar_usuario();
+    
+    agregar_usuario();
 }
 else if(ocupacion == "jubilado"){
     limite_prestamo = 30000
-    informar_usuario();
+    agregar_usuario();
 }
 else if(ocupacion == "empleado"){
     limite_prestamo = 60000
-    informar_usuario();
+    agregar_usuario();
 }
 else if(ocupacion == "desocupado"){
     limite_prestamo = 10000
-    informar_usuario();
+    agregar_usuario();
 }
 else if(ocupacion == "empresario"){
     limite_prestamo = 100000
-    informar_usuario();
+    agregar_usuario();
 }
 else if(ocupacion == "monotributista"){
-    limite_prestamo = 40000;
-    informar_usuario();
+    limite_prestamo = 400000
+    agregar_usuario();
 }
 else{alert("error");
     
 }
-numero = parseInt(prompt("ingresa el monto deseado"));
+}
+if( i = 3 ){
+console.log(lista_usuarios)
+}
+else{console.log("error")}
