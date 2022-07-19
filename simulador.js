6eb010ae4a74d121dd203e2000b2ebf150424fc0
mauricio gallardo <gonzaloegallardo@gmail.com>
2022-07-19 11:47:04 -0300

    agregando eventos

--- simulador.js
+++ simulador.js
@@ -1,53 +1,59 @@
-let limite_prestamo = 0
-class usuario{
-    constructor(nombre, apellido,ocupacion){
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.ocupacion = ocupacion;
-    }
-}
-function agregar_usuario(){
-    console.log("hola ", this.nombre , this.apellido  ,"tu ocupación es ", ocupacion,  " por lo tanto tu limite de prestamo es de :" ,limite_prestamo)
-    let nuevo_usuario = new usuario(this.nombre, this.apellido, this.ocupacion)
-    lista_usuarios.push (nuevo_usuario)
-}
-let lista_usuarios = [];
+ let empleado = document.getElementById("empleado");
+let  monto_maximo = document.getElementById("monto_maximo");
+("dinero");
 
-for(let i=0 ; i < 3; i++){
-this.nombre = prompt("ingrese su nombre")
-this.apellido = prompt("ingrese su apellido")
-this.ocupacion = prompt("ingrese su ocupacion")
 
-if(ocupacion == "estudiante"){
-    limite_prestamo = 20000;
+
+
+empleado.addEventListener("click", function(){
+    console.log("hola empleado")
+    alert("tu monto maximo es de : $70.000")
+    monto_maximo.innerHTML = ("monto maximo : $70.000")
+
     
-    agregar_usuario();
-}
-else if(ocupacion == "jubilado"){
-    limite_prestamo = 30000
-    agregar_usuario();
-}
-else if(ocupacion == "empleado"){
-    limite_prestamo = 60000
-    agregar_usuario();
-}
-else if(ocupacion == "desocupado"){
-    limite_prestamo = 10000
-    agregar_usuario();
-}
-else if(ocupacion == "empresario"){
-    limite_prestamo = 100000
-    agregar_usuario();
-}
-else if(ocupacion == "monotributista"){
-    limite_prestamo = 400000
-    agregar_usuario();
-}
-else{alert("error");
-    
-}
-}
-if( i = 3 ){
-console.log(lista_usuarios)
-}
-else{console.log("error")}
+
+})
+let desempleado = document.getElementById("desempleado");
+
+
+
+desempleado.addEventListener("click", function(){
+console.log("hola desempleado")
+alert("tu monto maximo es de : $30.000")
+monto_maximo.innerHTML = ("monto maximo : $30.000")
+})
+let jubilado = document.getElementById("jubilado");
+
+
+
+jubilado.addEventListener("click", function(){
+console.log("hola jubilado")
+alert("tu monto maximo es de : $50.000");
+monto_maximo.innerHTML = ("monto maximo : $50.000")
+})
+let monotributista = document.getElementById("monotributista");
+
+
+
+monotributista.addEventListener("click", function(){
+console.log("hola monotributista")
+alert("tu monto maximo es de : $60.000")
+monto_maximo.innerHTML = ("monto maximo : $60.000")
+})
+let empresario = document.getElementById("empresario");
+
+
+
+empresario.addEventListener("click", function(){
+console.log("hola empresario")
+alert("tu monto maximo es de : $100.000")
+})
+let estudiante = document.getElementById("estudiante");
+
+
+
+estudiante.addEventListener("click", function(){
+console.log("hola estudiante")
+alert("tu monto maximo es de : $40.000")
+monto_maximo.innerHTML = ("monto maximo : $40.000")
+})
