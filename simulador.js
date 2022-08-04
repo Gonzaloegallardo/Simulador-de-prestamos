26f4785e4f2bdfdf211494733ab8b224945fa015
gonza gallardo <gonzaloegallardo@gmail.com>
2022-08-04 11:54:37 -0300

    agregando comentarios

--- simulador.js
+++ simulador.js
@@ -1,3 +1,4 @@
+//obteniendo elementos// 
 let monto = document.getElementById("monto");
 let tiempo = document.getElementById("tiempo");
 let interes = document.getElementById("interes");
@@ -6,23 +7,23 @@ let llenar_tabla = document.querySelector("#lista-tabla tbody")
 
 btn_calcular.addEventListener("click", () => {
     calcular_cuota(monto.value, interes.value, tiempo.value);
-    let fallo = (calcular_cuota.incluides(String) ? true : false
-    fallo ? alert("error") : alert("bien")
+    
 });
 
 
 function calcular_cuota(monto, interes, tiempo){
-    
+    //metodo para que una vez que se ingresen datos nuevos, se renueve la lista.//
     while(llenar_tabla.firstChild){
     llenar_tabla.removeChild(llenar_tabla.firstChild);
 }
 
-
+//obteniendo fechas mediante la libreria moment.js//
 let fechas = [];
 let fecha_actual = Date.now();
 let mes_actual = moment(fecha_actual);
 mes_actual.add(1, "month");
 let pagoInteres = 0, pagoCapital = 0; cuota = 0;
+//calculando las cuotas segun el calculo del metodo de amortizacion frances//
 cuota = monto *(Math.pow(1+interes/100, tiempo)*interes/100)/(Math.pow(1+interes/100, tiempo)-1);
 
 
@@ -32,10 +33,10 @@ for(let i = 1; i <= tiempo; i++) {
     pagoCapital = cuota - pagoInteres;
     monto = parseFloat(monto-pagoCapital);
 
-
+    //aplicando la libreria para que muestre mes por mes hasta finalizar el prestamo//
     fechas[i] = mes_actual.format('DD-MM-YYYY');
     mes_actual.add(1, "month");
-
+//creando la lista con los datos //
 let row = document.createElement("tr");
 row.innerHTML = `
 <td>${fechas[i]}</td>
